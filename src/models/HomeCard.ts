b652527b1d721e36f76128dbe61d04d6b0b5eade
jassh <jassh4u@gmail.com>
2026-02-28 17:13:02 +0530

    changes in Homecard.ts for image upload

--- src/models/HomeCard.ts
+++ src/models/HomeCard.ts
@@ -14,7 +14,7 @@ const HomeCardSchema = new Schema<IHomeCard>(
         title: { type: String, required: true },
         description: { type: String, required: true },
         buttonText: { type: String, required: true },
-        image: { type: String, required: true },
+        image: { type: String },
         cardColor: { type: String, required: true },
         isActive: { type: Boolean, default: true },
     },
